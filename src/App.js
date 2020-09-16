3f1212d93cc9ef297c4dd190138259c84a90b572
John Hinderer <john.hinderer16@gmail.com>
2020-09-16 15:22:58 -0500

    spread operator

--- src/App.js
+++ src/App.js
@@ -15,13 +15,37 @@ class App extends Component {
     },
   }
 
+  onChangePizza = (event) => {
+    let newPizzaTopping = event.target.value;
+    console.log('changed pizza topping:', newPizzaTopping);
+
+    // Mutating the user object
+    // Mutating the state
+    // Don't do this, react won't know to render the user
+    // this.state.user.pizzaTopping = newPizzaTopping;
+
+    let oldUser = this.state.user;
+    let newUser = {
+      ...oldUser,
+      pizzaTopping: newPizzaTopping
+    }
+
+
+    this.setState({
+      // Create a new object for out state
+      // aka "Immutable" state
+      user: newUser
+    });
+  }
+
   render() {
+    console.log('changed pizza topping', this.state);
     return (
       <div>
         <h1>{this.state.cohortName} Learns State with Objects</h1>
 
         <div>
-          {this.state.salutation}! My name is {this.state.user.name}.
+          {this.state.salutation}
         </div>
 
         <ul>
@@ -29,6 +53,9 @@ class App extends Component {
           <li>I live in {this.state.user.whereILive}</li>
           <li>My favorite pizza topping is {this.state.user.pizzaTopping}</li>
         </ul>
+
+        <h4>Edit user</h4>
+        <input type="text" placeholder="Favorite Pizza Topping" onChange={this.onChangePizza} />
       </div>
     );
   }
